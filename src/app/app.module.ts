cd799f903c3865edfc60ba96e5f0511c90af3ce9
NamnvUET <nguyenvannamk59@gmail.com>
2017-09-05 14:16:57 +0700

    demo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Mytest1componentComponent } from './mytest1component/mytest1component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Mytest1componentComponent
   ],
   imports: [
     BrowserModule
