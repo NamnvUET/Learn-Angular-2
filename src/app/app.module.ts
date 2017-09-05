b636ca861cefe516b2082a9160da7d035cc6daba
NamnvUET <nguyenvannamk59@gmail.com>
2017-09-05 16:51:29 +0700

    update how to use a service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Mytest1componentComponent } from './mytest1component/mytest1component.component';
+import { MyListStudentComponent } from './my-list-student/my-list-student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Mytest1componentComponent
+    Mytest1componentComponent,
+    MyListStudentComponent
   ],
   imports: [
     BrowserModule
